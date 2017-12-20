5cf2a821a9570004716e8e35d7d6b820bb2fec2a
Brian Holley <brian.holley@hotmail.com>
2017-12-20 15:15:56 -0800

    Add automatic tests

--- test.js
+++ test.js
@@ -1,19 +1,16 @@
-// var fs = require('fs')
-// var os = require('os')
+var fs = require('fs')
+var os = require('os')
 var path = require('path')
 require('use-strict')
 
 var mosaick = require('./imagemosaick')
 
-let tileSize = 16
-var tileset = new mosaick.Tileset(
-    path.join('.', 'test', 'tileset'),
-    tileSize)
+var tileset = new mosaick.Tileset(path.join('.', 'test', 'tileset'), 8)
 
-// Test usage: test <input> <output>
-if (process.argv.length >= 5 && process.argv[2] === 'test') {
-  let input = process.argv[3]
-  let output = process.argv[4]
+// Test usage: node test <input> <output>
+if (process.argv.length >= 4) {
+  let input = process.argv[2]
+  let output = process.argv[3]
   console.log(`MANUAL TEST MODE: ${input} => ${output}`)
 
   mosaick.generate(input, output, tileset)
@@ -22,7 +19,18 @@ if (process.argv.length >= 5 && process.argv[2] === 'test') {
 } else {
   console.log(`UNIT TEST MODE`)
 
-//   let outdir = path.join(os.tmpdir(), 'imagemosaicktest', new Date().toTimeString())
-
-  // TODO: Add automatic tests
+  let srcFolder = path.join('.', 'test', 'color')
+  let outFolder = fs.mkdtempSync(path.join(os.tmpdir(), 'imagemosaicktest-'))
+  let files = fs.readdirSync(srcFolder)
+  let generators = files.map((f) => {
+    console.log(`Generating mosaic for ${f}`)
+    let inFile = path.join(srcFolder, f)
+    let outFile = path.join(outFolder, f)
+    return mosaick.generate(inFile, outFile, tileset)
+  })
+  Promise.all(generators).then(() => {
+    console.log(`PASS`)
+  }).catch(err => {
+    console.log(`FAIL: ${err}`)
+  })
 }
